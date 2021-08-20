1cf66656b952cbc31e4c4dc9fe14ebc9e3782aee
bot-Geras <primeoa101@gmail.com>
2021-08-20 15:51:58 +0300

    first commit

---
+++ app/src/main/java/com/example/patapp/Common/SplashScreen.java
@@ -0,0 +1,63 @@
+package com.example.patapp.Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.patapp.R;
+import com.example.patapp.User.PatientDashboard;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+
+    // variables
+
+    ImageView backgroundImage;
+    TextView poweredByLine;
+
+    //animation
+
+    Animation sideAnim, bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+        //hooks
+        backgroundImage = findViewById(R.id.background_image);
+        poweredByLine = findViewById(R.id.powered_by_line);
+
+        //animations
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+
+        //set animation on elements
+        backgroundImage.setAnimation(sideAnim);
+        poweredByLine.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, PatientDashboard.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },SPLASH_TIMER);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/patapp/User/PatientDashboard.java
@@ -0,0 +1,16 @@
+package com.example.patapp.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.patapp.R;
+
+public class PatientDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_dashboard);
+    }
+}
